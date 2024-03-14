56ed575bc77c5283193836f436620a8adfd2436c
Alireza Anbari <alireza.anbary@gmail.com>
2024-03-14 18:59:32 +0330

    Implement product_detal page
    1- change cover of home page.
    2- The situation of product out of stock.

--- store/models.py
+++ store/models.py
@@ -5,7 +5,6 @@ from category.models import Category
 from accounts.models import Account
 
 
-
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -15,9 +14,16 @@ class Product(models.Model):
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    
+
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
-    
+
+    def get_url(self):
+        # return reverse('product_detail', args=[self.category__slug, self.slug])
+        return reverse('product_detail', kwargs={
+            'category_slug': self.category.slug,
+            'product_slug': self.slug,
+        })
+
     def __str__(self):
         return self.product_name

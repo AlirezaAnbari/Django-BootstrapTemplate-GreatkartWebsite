d8363be97f7da145d63f7c95c2f2e7f36af705aa
Alireza Anbari <alireza.anbary@gmail.com>
2024-03-28 16:24:05 +0330

    Registration
    - Making view and editing model form.
    - clean methods for check password and email

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.core.files.base import File
 from django.db.models.base import Model
 from django.forms.utils import ErrorList
- 
+
 from .models import Account
  
  
@@ -31,3 +31,32 @@ class RegistrationForm(forms.ModelForm):
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
     
+    # def clean(self):
+    #     cleaned_data = super(RegistrationForm, self).clean()
+    #     password = cleaned_data.get('password')
+    #     confirm_password = cleaned_data.get('confirm_password')
+        
+    #     if password != confirm_password:
+    #         raise forms.ValidationError(
+    #             "password does not match!"
+    #         )
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        model = self.Meta.model
+        user = model.objects.filter(email__iexact=email)
+        
+        if user.exists():
+            raise forms.ValidationError("A user with that email already exists!")
+        
+        return self.cleaned_data.get('email')
+
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        confrim_password = self.data.get('confirm_password')
+        
+        if password != confrim_password:
+            raise forms.ValidationError("Passwords do not match")
+
+        return self.cleaned_data.get('password')

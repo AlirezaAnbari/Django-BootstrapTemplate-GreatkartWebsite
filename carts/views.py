f82cbaed67ed67c0396e04cb603a2e8b9e8761fd
Alireza Anbari <alireza.anbary@gmail.com>
2024-04-06 11:16:10 +0330

    Fix remove and cart decrement functions in views.

--- carts/views.py
+++ carts/views.py
@@ -163,18 +163,23 @@ def add_cart(request, product_id):
             
         return redirect('cart')
 
-
+# Remove one item
 def remove_cart(request, product_id, cart_item_id):
-    cart = Cart.objects.get(cart_id = _cart_id(request))
     product = get_object_or_404(Product, id=product_id)
     
     try:
-        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+        if request.user.is_authenticated:
+            cart_item = CartItem.objects.get(product=product, user=request.user, id=cart_item_id)
+        else:
+            cart = Cart.objects.get(cart_id = _cart_id(request))
+            cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+            
         if cart_item.quantity > 1:
             cart_item.quantity -= 1
             cart_item.save()
         else:
             cart_item.delete()
+            
     except:
         pass
         
@@ -182,9 +187,13 @@ def remove_cart(request, product_id, cart_item_id):
 
 
 def remove_cart_item(request, product_id, cart_item_id):
-    cart = Cart.objects.get(cart_id = _cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)    
+    
+    if request.user.is_authenticated:
+        cart_item = CartItem.objects.get(product=product, user=request.user, id=cart_item_id) 
+    else:
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)    
     
     cart_item.delete()
     

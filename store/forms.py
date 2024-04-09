b7260ed95859ecc130e9218d6e28975d4f2a6691
Alireza Anbari <alireza.anbary@gmail.com>
2024-04-09 21:07:30 +0330

    Making rating stars, applying  HTML and CSS code.

---
+++ store/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from .models import ReviewRating
+
+
+class ReviewForm(forms.Form):
+    
+    class Meta:
+        model = ReviewRating
+        fields = [
+            'subject', 'review', 'rating'
+        ]

027cd37431a62f6cb673490cd0f48e61fbeee036
Alireza Anbari <alireza.anbary@gmail.com>
2024-04-09 17:28:54 +0330

    Review and Rating model.

--- store/admin.py
+++ store/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import (
     Product,
     Variation,
+    ReviewRating
 )
 
 
@@ -25,3 +26,4 @@ class VariationAdmin(admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
+admin.site.register(ReviewRating)

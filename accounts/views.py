ea7a3d257170871060cc3ae48a0ddc94ea6384e1
Alireza Anbari <alireza.anbary@gmail.com>
2024-04-11 14:58:37 +0330

    Dashboard page edits.

--- accounts/views.py
+++ accounts/views.py
@@ -21,6 +21,7 @@ from .models import Account
 from .utils import email_token_generator
 from carts.models import Cart, CartItem
 from carts.views import _cart_id
+from orders.models import Order
 
 
 def register(request):
@@ -176,7 +177,14 @@ def activate(request, uidb64, token):
 
 @login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
+    orders_count = orders.count()
+    
+    context = {
+        'orders_count': orders_count,
+    }
+    
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def forgot_password(request):

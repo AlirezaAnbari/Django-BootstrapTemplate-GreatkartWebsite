94473f21e9c99b973a581b3f839cd296f8a775b5
Alireza Anbari <alireza.anbary@gmail.com>
2024-04-11 18:12:02 +0330

    Create UserProfile Model.

--- accounts/models.py
+++ accounts/models.py
@@ -75,3 +75,21 @@ class Account(AbstractBaseUser):
 
     def has_module_perms(self, add_label):
         return True
+    
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(Account, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(blank=True, upload_to='userprofile')
+    
+    address_line_1 = models.CharField(max_length=100, blank=True)
+    address_line_2 = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=20, blank=True)
+    state = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=20, blank=True)
+    
+    def full_address(self):
+        return f'{self.address_line_1} {self.address_line_2}'
+    
+    def __str__(self):
+        return self.user.first_name
+    

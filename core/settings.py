a91a9489b0ed6f3a93cb4ce85ec988cb16afc73c
Alireza Anbari <alireza.anbary@gmail.com>
2024-04-14 16:27:35 +0330

    Secure admin url

--- core/settings.py
+++ core/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     
-    # my app
+    # app
     "category",
     "accounts",
     "store",
--- core/urls.py
+++ core/urls.py
@@ -21,11 +21,14 @@ from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from decouple import config
 
 from . import views
 
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path(config("LOGIN_URL"), admin.site.urls),
+    
     path("accounts/", include("accounts.urls")),
     path("", views.home, name="home"),
     path("store/", include("store.urls")),
@@ -33,5 +36,6 @@ urlpatterns = [
     path("orders/", include("orders.urls")),
     
 ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

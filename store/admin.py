991287cbb46298dae917d36d528a2c91e085f0a3
Alireza Anbari <alireza.anbary@gmail.com>
2024-04-14 08:52:31 +0330

    Product Gallery model with image preview.

--- store/admin.py
+++ store/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
+import admin_thumbnails
 
 from .models import (
     Product,
     Variation,
-    ReviewRating
+    ReviewRating,
+    ProductGallery,
 )
 
-
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
+    
+    
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'product_name', 'price', 'stock', 'category', 'modified_date', 'is_available',
     )
     prepopulated_fields = {'slug': ('product_name',)}
+    inlines = [ProductGalleryInline]
 
 
 class VariationAdmin(admin.ModelAdmin):
@@ -27,3 +35,4 @@ class VariationAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
 admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
